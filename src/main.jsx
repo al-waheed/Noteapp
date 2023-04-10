11cac96395fe73894cfd7b41abd5536b97bc8b03
morenikeji ajisegiri <ajisegirimorenikeji@gmail.com>
2023-04-10 20:45:18 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,19 @@ import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { AuthContextProvider } from "./context/AuthContext"
+import { AuthContextProvider } from "./context/AuthContext";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <AuthContextProvider>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <AuthContextProvider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
     </AuthContextProvider>
   </React.StrictMode>
 );
