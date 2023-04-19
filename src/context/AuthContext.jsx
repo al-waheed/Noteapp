3c107bdbc7405f9a35322bc986b470ed73d97ffb
morenikeji ajisegiri <ajisegirimorenikeji@gmail.com>
2023-04-19 13:45:40 +0100

    update

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,41 +1,28 @@
 import { useContext, createContext, useEffect, useState } from 'react';
-
-// Import the necessary functions and variables from the Firebase authentication module and our Firebase configuration
 import {
   GoogleAuthProvider,
+  signInWithPopup,
   signInWithRedirect,
   signOut,
   onAuthStateChanged,
 } from 'firebase/auth';
-import { auth } from "../firebase";
+import { auth } from '../firebase';
 
-// Create a new context object using the createContext() function
-export const AuthContext = createContext();
+const AuthContext = createContext();
 
-// Define a new component that will wrap the rest of the app and provide authentication functionality
 export const AuthContextProvider = ({ children }) => {
-
-  // Define a new state variable called 'user' and initialize it to an empty object
   const [user, setUser] = useState({});
 
-  // Define a function called 'googleSignIn' that will initiate the Google sign-in flow when called
-  // Create a new instance of the GoogleAuthProvider class
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
+    // signInWithPopup(auth, provider);
     signInWithRedirect(auth, provider)
   };
 
-  // Define a function called 'logOut' that will sign the user out of their Firebase account when called
-  // Call the signOut() function from the Firebase authentication module, passing in the 'auth' variable as an argument
   const logOut = () => {
-    signOut(auth)
+      signOut(auth)
   }
 
-  // Use the useEffect() hook to listen for changes in the user's authentication state
-  // Call the onAuthStateChanged() function from the Firebase authentication module, passing in the 'auth' variable and a callback function that will be called whenever the user's authentication state changes
-  // Set the 'user' state variable to the current user object
-  // Log the current user object to the console
-  // Return a cleanup function that will unsubscribe from the onAuthStateChanged() listener when the component unmounts
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
@@ -46,7 +33,6 @@ export const AuthContextProvider = ({ children }) => {
     };
   }, []);
 
-  // Render the AuthContext.Provider component and pass in an object with the 'googleSignIn', 'logOut', and 'user' functions/variables as values
   return (
     <AuthContext.Provider value={{ googleSignIn, logOut, user }}>
       {children}
@@ -54,8 +40,6 @@ export const AuthContextProvider = ({ children }) => {
   );
 };
 
-// Define a new component called 'UserAuth' that will allow other components to access the 'googleSignIn', 'logOut', and 'user' functions/variables from the AuthContextProvider
-// Use the useContext() hook to access the current value of the AuthContext object
 export const UserAuth = () => {
   return useContext(AuthContext);
 };
